528c3d620b362b134872582598810e926d1f8536
Konstantin Stepanov <me@kstep.me>
2012-08-06 18:08:17 +0300

    Changed character at EOL if no LF to nice Unicode symbol

--- prompt.zsh
+++ prompt.zsh
@@ -2,4 +2,6 @@
 #export RPROMPT=$'[%{$fg_bold[red]%}%5(~.….)%4~%{$fg_no_bold[cyan]%}${vcs_info_msg_0_}%{$fg_bold[magenta]%}:%l%{$reset_color%}]'
 export PROMPT=$'%{$fg_no_bold[cyan]%}[%{$fg_bold[red]%}%5(~.….)%4~%{$fg_no_bold[cyan]%}${vcs_info_msg_0_}%{$fg_bold[magenta]%}:%l%{$fg_no_bold[cyan]%}]%{$reset_color%}%(0?..%{$fg_bold[red]%}[%?]%{$reset_color%}) '
 
+export PROMPT_EOL_MARK=$'%{$bg[cyan]%}%{$fg_no_bold[black]%}↲%{$reset_color%}'
+
 # vim: filetype=zsh
