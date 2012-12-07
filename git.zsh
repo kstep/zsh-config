672f22b8f4854894755a4cec5a392bc78541b0db
Konstantin Stepanov <me@kstep.me>
2012-12-07 12:27:01 +0300

    New command prompt format with Unicode typographic characters

--- git.zsh
+++ git.zsh
@@ -5,11 +5,11 @@ zstyle ':vcs_info:*' enable git
 
 # Stage and unstage markers.
 zstyle ':vcs_info:*' unstagedstr '?'
-zstyle ':vcs_info:*' stagedstr '+'
+zstyle ':vcs_info:*' stagedstr '!'
 
 # Banners format.
-zstyle ':vcs_info:*' formats '@%b%u%c'
-zstyle ':vcs_info:*' actionformats '@%b%u%c:%a'
+zstyle ':vcs_info:*' formats ' %b%u%c'
+zstyle ':vcs_info:*' actionformats ' %b%u%c:%a'
 zstyle ':vcs_info:*' check-for-changes true
 
 autoload -Uz vcs_info
--- prompt.zsh
+++ prompt.zsh
@@ -19,7 +19,7 @@
 
 #export RPROMPT=$'%(0?..%{$fg_bold[red]%}[%?]%{$reset_color%})[ %{$fg_bold[red]%}%5(~.….)%4~%{$fg_no_bold[cyan]%}${vcs_info_msg_0_}%{$reset_color%} ][ %{$fg_bold[green]%}%n@%{$fg_bold[blue]%}%m%{$reset_color%} %{$fg_bold[magenta]%}%l%{$reset_color%} ]'
 #export RPROMPT=$'[%{$fg_bold[red]%}%5(~.….)%4~%{$fg_no_bold[cyan]%}${vcs_info_msg_0_}%{$fg_bold[magenta]%}:%l%{$reset_color%}]'
-export PROMPT=$'%{$fg_no_bold[cyan]%}[%{$fg_bold[red]%}%5(~.….)%4~%{$fg_no_bold[cyan]%}${vcs_info_msg_0_}%{$fg_bold[magenta]%}:%l%{$fg_no_bold[cyan]%}]%{$reset_color%}%(0?..%{$fg_bold[red]%}[%?]%{$reset_color%}) '
+export PROMPT=$'%{$fg_no_bold[cyan]%} — %{$fg_bold[green]%}%5(~.….)%4~%{$fg_no_bold[cyan]%}${vcs_info_msg_0_}%{$fg_bold[magenta]%} %l%{$fg_no_bold[cyan]%}%{$reset_color%}%(0?..%{$fg_bold[red]%}[%?]%{$reset_color%}): '
 
 #
 # This is a replacement for EOL sign if output is missing last NL character.
