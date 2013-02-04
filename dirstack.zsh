6494b1b41f58336ee183e535956e1a1ad43fe179
Konstantin Stepanov <me@kstep.me>
2013-02-04 16:36:53 +0300

    Add dirstack persistance hooks
    See http://chneukirchen.org/blog/archive/2012/02/10-new-zsh-tricks-you-may-not-know.html

---
+++ dirstack.zsh
@@ -0,0 +1,17 @@
+# The script stores dirs stack into given file and restore it on shell start.
+
+# First we configure this feature.
+DIRSTACKSIZE=9
+DIRSTACKFILE=~/.zdirs
+
+# The load dirs into stack from given file.
+if [[ -f $DIRSTACKFILE && $#dirstack -eq 0 ]]; then
+    dirstack=( ${(f)"$(< $DIRSTACKFILE)"} )
+    [[ -d $dirstack[1] ]] && cd $dirstack[1] && cd $OLDPWD
+fi
+
+# At last we add a function to store dirs stack on dir change.
+function chpwd () {
+    print -l $PWD ${(u)dirstack} > $DIRSTACKFILE
+}
+
