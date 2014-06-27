9597ae122306c28f716d0538cbe8774064f38a40
Konstantin Stepanov <me@kstep.me>
2014-06-27 13:27:29 +0300

    add autogenerated airline prompt
    See https://github.com/edkolev/promptline.vim
    
    Config:
    
        let g:promptline_preset = {
                    \'a': [ promptline#slices#cwd() ],
                    \'b': ['${vcs_info_msg_0_}'],
                    \'c': ['%l'],
                    \'warn': [ promptline#slices#last_exit_code() ],
                    \'options': {
                        \'left_sections': [ 'a', 'b', 'c', 'warn' ],
                        \'left_only_sections': [ 'a', 'b', 'c', 'warn' ],
                        \}
                    \}

---
+++ plprompt.zsh
@@ -0,0 +1,164 @@
+#
+# This shell prompt config file was created by promptline.vim
+#
+
+function __promptline_last_exit_code {
+
+  [[ $last_exit_code -gt 0 ]] || return 1;
+
+  printf "%s" "$last_exit_code"
+}
+function __promptline_ps1 {
+  local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
+
+  # section "a" header
+  slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "a" slices
+  __promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # section "b" header
+  slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "b" slices
+  __promptline_wrapper "${vcs_info_msg_0_}" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # section "c" header
+  slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "c" slices
+  __promptline_wrapper "%l" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # section "warn" header
+  slice_prefix="${warn_bg}${sep}${warn_fg}${warn_bg}${space}" slice_suffix="$space${warn_sep_fg}" slice_joiner="${warn_fg}${warn_bg}${alt_sep}${space}" slice_empty_prefix="${warn_fg}${warn_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "warn" slices
+  __promptline_wrapper "$(__promptline_last_exit_code)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # close sections
+  printf "%s" "${reset_bg}${sep}$reset$space"
+}
+function __promptline_cwd {
+  local dir_limit="3"
+  local truncation="⋯"
+  local first_char
+  local part_count=0
+  local formatted_cwd=""
+  local dir_sep="  "
+  local tilde="~"
+
+  local cwd="${PWD/#$HOME/$tilde}"
+
+  # get first char of the path, i.e. tilde or slash
+  [[ -n ${ZSH_VERSION-} ]] && first_char=$cwd[1,1] || first_char=${cwd::1}
+
+  # remove leading tilde
+  cwd="${cwd#\~}"
+
+  while [[ "$cwd" == */* && "$cwd" != "/" ]]; do
+    # pop off last part of cwd
+    local part="${cwd##*/}"
+    cwd="${cwd%/*}"
+
+    formatted_cwd="$dir_sep$part$formatted_cwd"
+    part_count=$((part_count+1))
+
+    [[ $part_count -eq $dir_limit ]] && first_char="$truncation" && break
+  done
+
+  printf "%s" "$first_char$formatted_cwd"
+}
+function __promptline_left_prompt {
+  local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
+
+  # section "a" header
+  slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "a" slices
+  __promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # section "b" header
+  slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "b" slices
+  __promptline_wrapper "${vcs_info_msg_0_}" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # section "c" header
+  slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "c" slices
+  __promptline_wrapper "%l" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # section "warn" header
+  slice_prefix="${warn_bg}${sep}${warn_fg}${warn_bg}${space}" slice_suffix="$space${warn_sep_fg}" slice_joiner="${warn_fg}${warn_bg}${alt_sep}${space}" slice_empty_prefix="${warn_fg}${warn_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "warn" slices
+  __promptline_wrapper "$(__promptline_last_exit_code)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # close sections
+  printf "%s" "${reset_bg}${sep}$reset$space"
+}
+function __promptline_wrapper {
+  # wrap the text in $1 with $2 and $3, only if $1 is not empty
+  # $2 and $3 typically contain non-content-text, like color escape codes and separators
+
+  [[ -n "$1" ]] || return 1
+  printf "%s" "${2}${1}${3}"
+}
+function __promptline_right_prompt {
+  local slice_prefix slice_empty_prefix slice_joiner slice_suffix
+
+  # section "warn" header
+  slice_prefix="${warn_sep_fg}${rsep}${warn_fg}${warn_bg}${space}" slice_suffix="$space${warn_sep_fg}" slice_joiner="${warn_fg}${warn_bg}${alt_rsep}${space}" slice_empty_prefix=""
+  # section "warn" slices
+  __promptline_wrapper "$(__promptline_last_exit_code)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; }
+
+  # close sections
+  printf "%s" "$reset"
+}
+function __promptline {
+  local last_exit_code="$?"
+
+  local esc=$'[' end_esc=m
+  if [[ -n ${ZSH_VERSION-} ]]; then
+    local noprint='%{' end_noprint='%}'
+  else
+    local noprint='\[' end_noprint='\]'
+  fi
+  local wrap="$noprint$esc" end_wrap="$end_esc$end_noprint"
+  local space=" "
+  local sep=""
+  local rsep=""
+  local alt_sep=""
+  local alt_rsep=""
+  local reset="${wrap}0${end_wrap}"
+  local reset_bg="${wrap}49${end_wrap}"
+  local a_fg="${wrap}38;5;7${end_wrap}"
+  local a_bg="${wrap}48;5;11${end_wrap}"
+  local a_sep_fg="${wrap}38;5;11${end_wrap}"
+  local b_fg="${wrap}38;5;7${end_wrap}"
+  local b_bg="${wrap}48;5;14${end_wrap}"
+  local b_sep_fg="${wrap}38;5;14${end_wrap}"
+  local c_fg="${wrap}38;5;14${end_wrap}"
+  local c_bg="${wrap}48;5;7${end_wrap}"
+  local c_sep_fg="${wrap}38;5;7${end_wrap}"
+  local warn_fg="${wrap}38;5;15${end_wrap}"
+  local warn_bg="${wrap}48;5;9${end_wrap}"
+  local warn_sep_fg="${wrap}38;5;9${end_wrap}"
+  if [[ -n ${ZSH_VERSION-} ]]; then
+    PROMPT="$(__promptline_left_prompt)"
+    #RPROMPT="$(__promptline_right_prompt)"
+  else
+    PS1="$(__promptline_ps1)"
+  fi
+}
+
+if [[ -n ${ZSH_VERSION-} ]]; then
+  if [[ ! ${precmd_functions[(r)__promptline]} == __promptline ]]; then
+    precmd_functions+=(__promptline)
+  fi
+else
+  if [[ ! "$PROMPT_COMMAND" == *__promptline* ]]; then
+    PROMPT_COMMAND='__promptline;'$'\n'"$PROMPT_COMMAND"
+  fi
+fi
