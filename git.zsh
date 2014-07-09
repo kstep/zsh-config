ea7a86e4afb51b3d666e3ebdd7e894d96d6f7ae4
Konstantin Stepanov <me@kstep.me>
2014-07-09 16:26:13 +0300

    remove branch symbol from git prompt

--- git.zsh
+++ git.zsh
@@ -8,8 +8,8 @@ zstyle ':vcs_info:*' unstagedstr '?'
 zstyle ':vcs_info:*' stagedstr '!'
 
 # Banners format.
-zstyle ':vcs_info:*' formats ' %b%u%c'
-zstyle ':vcs_info:*' actionformats ' %b%u%c:%a'
+zstyle ':vcs_info:*' formats ' %b%u%c'
+zstyle ':vcs_info:*' actionformats ' %b%u%c:%a'
 zstyle ':vcs_info:*' check-for-changes true
 
 autoload -Uz vcs_info
