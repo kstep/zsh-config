81187ab4f876508924035e0a5fad32a37ce283d2
Konstantin Stepanov <me@kstep.me>
2014-07-09 16:25:16 +0300

    add vim mode

--- keybindings.zsh
+++ keybindings.zsh
@@ -4,10 +4,6 @@
 autoload -Uz zkbd
 source $HOME/.zkbd/$TERM-$VENDOR-$OSTYPE || DISPLAY= zkbd
 
-# I like **Vim** very much, but modal editing in command line
-# feels a little masochistic, hence **Emacs** mode here.
-bindkey -e
-
 # `Home` and `End` keys are used from time to time.
 bindkey ${key[Home]} vi-beginning-of-line
 bindkey ${key[End]} vi-end-of-line
@@ -17,7 +13,4 @@ bindkey ${key[End]} vi-end-of-line
 # by the prefix.
 bindkey ${key[Up]} up-line-or-search
 bindkey ${key[Down]} down-line-or-search
-
-# Standard history search keybinding.
-bindkey "^R" history-incremental-search-backward
 
---
+++ vimode.zsh
@@ -0,0 +1,25 @@
+
+# I love **Vim** very much, so I use it as main edit mode in zsh
+bindkey -v
+
+# Standard history search keybinding.
+bindkey '^R' history-incremental-search-backward
+bindkey '^P' up-history
+bindkey '^N' down-history
+
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
+
+# Reduce timeout to switch to command mode to 0.1 sec
+export KEYTIMEOUT=1
+
+# Add right-hand mode indicator to distinguish **Vim** modes
+function zle-line-init zle-keymap-select {
+    VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]% %{$reset_color%}"
+    RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/} $EPS1"
+    zle reset-prompt
+}
+
+# Hook up editing event to change right-hand prompt
+zle -N zle-line-init
+zle -N zle-keymap-select
